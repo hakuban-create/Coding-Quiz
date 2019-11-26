bd9497afe4eed058d9f425f8e31c15a3f5f95217
Khaliunaa Baasandorj <khaliunaabaasandorj@Khaliunaas-MacBook-Pro-2.local>
2019-11-26 00:45:40 -0500

    added html and css

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
-
     
+    var timeEl=$("#time");
+    var startBtn=$("#start-btn");
+    var highScore=$("#high-score");
+    
+    var remainingTime=75;
+
+
+
+
+function start(){
+    setInterval(function(){
+    if(remainingTime==0){
+        document.clearInterval();
+    }
+        remainingTime--;
+        timeEl.html(remainingTime);
+    },1000);
+}
+
+
+
+
+
+
+
+
+startBtn.on("click",start);
 
 });
